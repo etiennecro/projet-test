1d95c30bd5dc897387e2349cb45ac1ef05e03c25
etiennecro <157416699+etiennecro@users.noreply.github.com>
2025-12-09 15:13:33 -0500

    Update termes.js

--- termes.js
+++ termes.js
@@ -2,7 +2,7 @@
 // Format: [Terme, Définition, Source, Autres termes, Notes]
 
 const LEX = [
-  ["Dossier de santé informatiser", "Dossier contenant certaines données médicales numérisées et stockées sur un serveur, qui permet de suivre l'état de santé d'un patient en consultation par un accès en ligne entre praticiens et établissements de santé.", "GDT", "Dossier Santé Québec ou DSQ", "Les principales données médicales accessibles en ligne sont les résultats des examens de laboratoire, les rapports d'imagerie médicale et la liste détaillée des médicaments prescrits au patient."]
+  ["Dossier de santé informatisé", "Dossier contenant certaines données médicales numérisées et stockées sur un serveur, qui permet de suivre l'état de santé d'un patient en consultation par un accès en ligne entre praticiens et établissements de santé.", "GDT", "Dossier Santé Québec ou DSQ", "Les principales données médicales accessibles en ligne sont les résultats des examens de laboratoire, les rapports d'imagerie médicale et la liste détaillée des médicaments prescrits au patient."]
   
   // Ajoutez vos 359 autres termes ici au même format
   // ["Terme", "Définition", "Source", "Autres termes", "Notes"],
