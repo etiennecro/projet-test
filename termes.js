db3e9a1d027d3c3859a70a04b5dc1ae62e5b16ec
etiennecro <157416699+etiennecro@users.noreply.github.com>
2025-12-09 15:16:14 -0500

    Update termes.js

--- termes.js
+++ termes.js
@@ -2,8 +2,14 @@
 // Format: [Terme, Définition, Source, Autres termes, Notes]
 
 const LEX = [
-  ["Dossier de santé informatisé", "Dossier contenant certaines données médicales numérisées et stockées sur un serveur, qui permet de suivre l'état de santé d'un patient en consultation par un accès en ligne entre praticiens et établissements de santé.", "GDT", "Dossier Santé Québec ou DSQ", "Les principales données médicales accessibles en ligne sont les résultats des examens de laboratoire, les rapports d'imagerie médicale et la liste détaillée des médicaments prescrits au patient."]
+  ["Dossier de santé informatisé", "Dossier contenant certaines données médicales numérisées et stockées sur un serveur, qui permet de suivre l'état de santé d'un patient en consultation par un accès en ligne entre praticiens et établissements de santé.", "GDT", "Dossier Santé Québec ou DSQ", "Les principales données médicales accessibles en ligne sont les résultats des examens de laboratoire, les rapports d'imagerie médicale et la liste détaillée des médicaments prescrits au patient."],
   
-  // Ajoutez vos 359 autres termes ici au même format
+  ["Emballage multidose individualisé", "Emballage contenant plusieurs médicaments identifiés, regroupés par heure d'administration et destinés à un usager spécifique.", "En collaboration avec l'APES.", "Sachet regroupé", ""],
+  
+  ["Emballage unitaire", "Emballage contenant une unité de médicament identifiée (ex. : un sachet comprenant un comprimé de furosémide 20 mg).", "En collaboration avec l'APES.", "Sachet unidose", ""],
+  
+  ["Enceinte de préparation stérile", "Équipement assurant un niveau de classification ISO 5 pour la qualité de l'air filtré HEPA sur les sites critiques exposés lors de la technique aseptique.", "OPQ.Norme 2014. 01; 2014", "Hotte à flux laminaire", ""]
+  
+  // Ajoutez vos 356 autres termes ici au même format
   // ["Terme", "Définition", "Source", "Autres termes", "Notes"],
 ];
